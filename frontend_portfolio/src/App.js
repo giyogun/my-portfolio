856e4a08a04ecdf9efbc498ce5e65bfbe364f03a
Godwin Iyogun <giyogun@gmail.com>
2022-04-14 11:42:37 +0100

    add containers(sections) and navbar with their style files

--- frontend_portfolio/src/App.js
+++ frontend_portfolio/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { About, Footer, Header, Skills, Testimonial, Work} from "./container"
+
+import "./App.scss";
 
 const App = () => {
-  return <div></div>;
+  return <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>;
 };
 
 export default App;
